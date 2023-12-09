f8b51ee290b3325e49eaa8aa805885799b51838d
MRD the Bocchi <126393669+mrd010@users.noreply.github.com>
2023-12-09 10:44:44 +0330

    Initial commit

---
+++ webpack/webpack.config.prod.js
@@ -0,0 +1,28 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  // output: {
+  //   library: 'myLibrary',
+  //   libraryTarget: 'umd',
+  //   umdNamedDefine: true,
+  // },
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+});
