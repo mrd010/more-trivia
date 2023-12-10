38bb2f45e81b3dde98f5a8c804dd07456f4fedd3
Gregar <126393669+mrd010@users.noreply.github.com>
2023-12-10 22:56:40 +0330

    Create tailwind style list for some elements

---
+++ src/scripts/Styles.js
@@ -0,0 +1,7 @@
+const styles = {
+  inputFieldStyle: 'w-full flex flex-col p-2 gap-1',
+  inputStyle: 'px-2 py-1 w-full rounded-md border-2 border-slate-900/5 bg-slate-400/20',
+  labelStyle: 'text-sm text-slate-950/75 font-medium ml-2',
+};
+
+export default styles;
