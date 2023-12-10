8c0fed79743d61f6b60dee7d78661dfbf23d2cc8
Gregar <126393669+mrd010@users.noreply.github.com>
2023-12-10 23:53:13 +0330

    Add initialize function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import './style.css';
+import './style/style.css';
+import initialLoad from './scripts/DisplayController';
+
+// const url = 'https://opentdb.com/api.php?amount=10';
+// async function test() {
+//   const response = await fetch(url, { mode: 'cors' });
+//   const rj = await response.json();
+//   console.log(rj);
+// }
+
+// test();
+initialLoad();
