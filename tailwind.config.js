a92de9aed7a8084fc56a9042950a273b1d5cf45a
Gregar <126393669+mrd010@users.noreply.github.com>
2023-12-12 22:18:03 +0330

    Add custom animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,html}'],
   theme: {
     extend: {},
+    animation: {
+      'pulse-fast': 'pulse 0.5s 3',
+      'ping-once': 'ping 1',
+    },
     fontFamily: {
       display: ['Passion One', 'sans-serif'],
     },
